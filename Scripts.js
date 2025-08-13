3e0719232527463ac08809a4e2db9d54dccf4d58
Rkaueee <134891824+Rkaueee@users.noreply.github.com>
2025-08-12 23:16:07 -0300

    Adicionado Linguagens e Codigos

---
+++ Scripts.js
@@ -0,0 +1,25 @@
+
+
+ const artistists = [
+    { Name : "henrique e juliano", Image : 'img/artista-henrique-juliano.jpg' } , 
+    { Name : "jorge e Mateus ", Image : 'img/artista-jorge-mateus.jpg' } ,
+    { Name : "Zé Neto e Cristiano ", Image : 'img/artista-ze-neto.jpg' } , 
+    { Name : "Gustavo Lima", Image : 'img/artista-gustavo-limma.jpg' } , 
+    { Name : "Luan Santana", Image : 'img/artista-luan-santana.jpg' } , 
+    { Name : "Matheus e Kauan", Image : 'img/artista-mateus-kauan.jpg' } , 
+    
+ ];
+
+
+ const Albms = [
+    { Name : "White Noise (Sleep e Relaxation Sounds ",artist: 'Sleepy Jhon', Image : 'img/album-white-noise.jpg' } , 
+    { Name : "O Ceu Explica Tudo(Ao Vivo)",artist: 'Henrique e Juliano ', Image : 'img/album-ceu-explica.jpg' } ,
+    { Name : "Nada Como um dia Apos o outro ",artist: 'Racionais', Image : 'img/album-racionais.jpg' } ,
+    { Name : "HIT ME HARD AND SOFT",artist: 'Billie Eilish', Image : 'img/album-hit-me.jpg' } , 
+    { Name : "CAJU ",artist: 'liniker', Image : 'img/album-caju.jpg' } , 
+    { Name : "Escandalo Intimo",artist: 'Luisa Sonza ', Image : 'img/album-escandalo.jpg' } ,
+ ];
+
+ 
+
+
